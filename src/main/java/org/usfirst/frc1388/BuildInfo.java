a2f37c41e91d4251a0e98adcfd30a282f6e1f695
DillonRego <dillonjrego17@gmail.com>
2018-10-25 20:45:13 -0700

    First Test 2019
    Successful Build

---
+++ src/main/java/org/usfirst/frc1388/BuildInfo.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc1388;
+public class BuildInfo {
+  public static final String GIT_VERSION = "???????";
+  public static final String GIT_BRANCH = "(no branch)";
+  public static final String GIT_STATUS = "";
+  public static final String BUILD_DATE = "Thu 10/25/2018 ";
+  public static final String BUILD_TIME = "08:41 PM";
+}
